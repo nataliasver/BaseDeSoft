79d05c6e492f5bbcfe73790c0ec22928680701a6
Natalia <brujinat@gmail.com>
2021-11-20 21:58:31 -0300

    Se agrega inicio de pedido

--- routes/backoffice.js
+++ routes/backoffice.js
@@ -2,14 +2,14 @@ function getLocale() {
     if (typeof window === 'undefined' || navigator == null || navigator.language == null) {
         return 'es-AR'
     }
-  
+
     if (navigator.languages != null) {
         return navigator.languages[0];
     }
-  
+
     return navigator.language
-  }
-  
+}
+
 
 class BackOffice {
 
@@ -17,9 +17,9 @@ class BackOffice {
         date = new Date(date);
         const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
         const locale = getLocale();
-    
+
         const dateStr = date.toLocaleDateString(locale, options);
-    
+
         return dateStr.charAt(0).toUpperCase() + dateStr.slice(1);
     }
     //Para la sesion
@@ -28,7 +28,7 @@ class BackOffice {
         const mypassword = 'unaclave'
         let unasession;
 
-       
+
 
         //---------- Sesiones y back office --------------
         app.get('/office', function (req, res) {
@@ -56,30 +56,59 @@ class BackOffice {
 
 
         // ---------------- Pedidos ----------------------
-        
+
         app.get('/backoffice', async function (req, res, next) {
             let pedidossql = "SELECT * FROM `pedidos` LEFT JOIN equipos ON pedidos.equipo_id_fk = equipos.equipo_id LEFT JOIN tiposequipos ON equipos.tipo_equipo_fk = tiposequipos.tipo_equipo_id LEFT JOIN marcasequipos ON equipos.marca_equipo_fk = marcasequipos.marca_equipo_id LEFT JOIN oficinas ON pedidos.oficina_id_fk = oficinas.oficina_id LEFT JOIN estadosreparacion ON pedidos.estado_id_fk = estadosreparacion.estado_id;"
+            let oficinassql = "SELECT * FROM oficinas";
             let equipossql = "SELECT * FROM equipos LEFT JOIN marcasequipos ON equipos.marca_equipo_fk = marcasequipos.marca_equipo_id LEFT JOIN tiposequipos ON equipos.tipo_equipo_fk = tiposequipos.tipo_equipo_id";
             let marcassql = "SELECT * FROM marcasequipos";
             let tipossql = "SELECT * FROM tiposequipos";
             let estadossql = "SELECT * FROM estadosreparacion";
             try {
                 const pedidos = await querynaty(pedidossql);
+                const oficinas = await querynaty(oficinassql);
                 const equipos = await querynaty(equipossql);
                 const marcas = await querynaty(marcassql);
                 const tipos = await querynaty(tipossql);
                 const estados = await querynaty(estadossql);
                 res.render("backoffice", {
                     equipos: equipos,
+                    oficinas: oficinas,
                     tipos: tipos,
                     marcas: marcas,
-                    pedidos: pedidos.map(pedido => ({...pedido, pedido_fecha_inicio: BackOffice._dateString(pedido.pedido_fecha_inicio)})),
+                    pedidos: pedidos.map(pedido => ({ ...pedido, pedido_fecha_inicio: BackOffice._dateString(pedido.pedido_fecha_inicio) })),
                     estados: estados,
                 });
             } catch (err) {
                 console.log(err);
             }
-         });
+        });
+
+        app.post("/savepedido", async function (req, res, next) {
+            const date = Date.now();
+            let date_ob = new Date(date);
+            let dateday = date_ob.getDate();
+            let month = date_ob.getMonth() + 1;
+            let year = date_ob.getFullYear();
+            let datetoday = year + "-" + month + "-" + dateday
+
+            let data = {
+                equipo_id_fk: req.body.equipo_id,
+                serial_number: req.body.serial_number,
+                oficina_id_fk: req.body.oficina_id,
+                pedido_problema: req.body.problema,
+                pedido_fecha_inicio: datetoday,
+                estado_id_fk: 1,
+            };
+            let pedidonuevosql = "INSERT INTO pedidos SET ?";
+            try {
+                const saveequiposql = await querynaty(pedidonuevosql, data);
+                res.redirect("/backoffice");
+            } catch (err) {
+                console.log(err);
+            }
+        });
+
         app.post('/deletepedido', async function (req, res, next) {
             let deletepedidosql = "DELETE FROM pedidos WHERE pedido_id=" + req.body.id + "";
             try {
@@ -88,9 +117,9 @@ class BackOffice {
             } catch (err) {
                 console.log(err);
             }
-         });
+        });
 
-        
+
         //To Do: Faltaria añadir pedido, editar pedido y cambio de estado
         //----------------- Equipos ----------------------
         app.get("/equipos", async function (req, res, next) {
@@ -115,8 +144,8 @@ class BackOffice {
             let equipossql =
                 "UPDATE equipos SET marca_equipo_fk='" +
                 req.body.marca_equipo_id +
-                "', tipo_equipo_fk='"+ req.body.tipo_equipo_id +
-                "', equipo_modelo='"+ req.body.equipo_modelo +
+                "', tipo_equipo_fk='" + req.body.tipo_equipo_id +
+                "', equipo_modelo='" + req.body.equipo_modelo +
                 "' WHERE equipo_id=" +
                 req.body.id;
             try {
@@ -131,10 +160,10 @@ class BackOffice {
                 equipo_modelo: req.body.equipo_modelo,
                 tipo_equipo_fk: req.body.tipo_equipo_id,
                 marca_equipo_fk: req.body.marca_equipo_id,
-                };
-                let equiponuevosql = "INSERT INTO equipos SET ?";
-                try {
-                const saveequiposql = await querynaty(equiponuevosql,data);          
+            };
+            let equiponuevosql = "INSERT INTO equipos SET ?";
+            try {
+                const saveequiposql = await querynaty(equiponuevosql, data);
                 res.redirect("/equipos");
             } catch (err) {
                 console.log(err);
@@ -169,10 +198,10 @@ class BackOffice {
         app.post("/savemarca", async function (req, res, next) {
             let data = {
                 marca_equipo: req.body.marca_equipo,
-                };
-                let marcanuevasql = "INSERT INTO marcasequipos SET ?";
-                try {
-                const savemarcassql = await querynaty(marcanuevasql,data);          
+            };
+            let marcanuevasql = "INSERT INTO marcasequipos SET ?";
+            try {
+                const savemarcassql = await querynaty(marcanuevasql, data);
                 res.redirect("/marcas");
             } catch (err) {
                 console.log(err);
@@ -180,7 +209,7 @@ class BackOffice {
         });
 
         // ------------ Tipos de equipos ------------------
-       
+
         app.get("/tipos", async function (req, res, next) {
             let tipossql = "SELECT * FROM tiposequipos";
             try {
@@ -208,10 +237,10 @@ class BackOffice {
         app.post("/savetipo", async function (req, res, next) {
             let data = {
                 tipo_equipo: req.body.tipo_equipo,
-                };
-                let tiponuevosql = "INSERT INTO tiposequipos SET ?";
-                try {
-                const savetipossql = await querynaty(tiponuevosql,data);          
+            };
+            let tiponuevosql = "INSERT INTO tiposequipos SET ?";
+            try {
+                const savetipossql = await querynaty(tiponuevosql, data);
                 res.redirect("/tipos");
             } catch (err) {
                 console.log(err);
@@ -234,9 +263,9 @@ class BackOffice {
             let oficinasql =
                 "UPDATE oficinas SET oficina_nombre='" +
                 req.body.oficina_nombre +
-                "', oficina_direccion='"+ req.body.oficina_direccion +
-                "', oficina_telefono='"+ req.body.oficina_telefono +
-                "', oficina_email='"+ req.body.oficina_email + 
+                "', oficina_direccion='" + req.body.oficina_direccion +
+                "', oficina_telefono='" + req.body.oficina_telefono +
+                "', oficina_email='" + req.body.oficina_email +
                 "' WHERE oficina_id=" + req.body.id;
             try {
                 const updateoficinasql = await querynaty(oficinasql);
@@ -245,16 +274,16 @@ class BackOffice {
                 console.log(err);
             }
         });
-        app.post("/saveoficina", async function (req, res, next) {        
+        app.post("/saveoficina", async function (req, res, next) {
             let data = {
                 oficina_nombre: req.body.oficina_nombre,
                 oficina_direccion: req.body.oficina_direccion,
                 oficina_telefono: req.body.oficina_telefono,
                 oficina_email: req.body.oficina_email,
-                };
-                let oficinanuevasql = "INSERT INTO oficinas SET ?";
-                try {
-                const saveoficinasql = await querynaty(oficinanuevasql,data);          
+            };
+            let oficinanuevasql = "INSERT INTO oficinas SET ?";
+            try {
+                const saveoficinasql = await querynaty(oficinanuevasql, data);
                 res.redirect("/oficinas");
             } catch (err) {
                 console.log(err);
